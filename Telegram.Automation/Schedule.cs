bcab30258a2b7d9959c6875e97a5d823c39ec219
Ladislav Šesták <ladislav.sestak@riganti.cz>
2024-11-06 23:59:24 +0000

    Default value of Schedule.IsActive set to true

--- Telegram.Automation/Schedule.cs
+++ Telegram.Automation/Schedule.cs
@@ -5,5 +5,5 @@ public class Schedule
     public string Name { get; set; } = "Unknown";
     public int Id { get; set; }
     public List<ScheduleItem> Plan { get; set; } = new();
-    public bool IsActive { get; set; } 
+    public bool IsActive { get; set; } = true; 
 }
